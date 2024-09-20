be4b3dfa3cf3b5192bd52bc0f80f0f641fb3577c
Denis-Turbatu <turbatudenis81@gmail.com>
2024-09-20 18:17:29 +0200

    feat: styled project for readability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { FormDataComponent } from './form-data/form-data.component';
+import { ProfileComponent } from "./profile/profile.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormDataComponent],
+  imports: [RouterOutlet, FormDataComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,8 @@
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import { NgModule } from '@angular/core';
 
 @NgModule({
@@ -8,6 +10,8 @@ import { NgModule } from '@angular/core';
         MatFormFieldModule,
         MatIconModule,
         MatInputModule,
+        MatButtonModule,
+        MatCardModule
     ]
 })
 export class MaterialModule { }
